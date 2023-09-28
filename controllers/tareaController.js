c1cee64c1b779524b5ea442274516ccb0f03de72
Mateo Rodriguez <mdavidrodriguez@unicesar.edu.co>
2023-09-28 15:10:52 -0500

    Añadiendo validaciones pruebas de integracion

--- controllers/tareaController.js
+++ controllers/tareaController.js
@@ -1,30 +1,68 @@
 import Proyecto from "../models/Proyecto.js";
 import Tarea from "../models/Tarea.js";
 
-const agregarTarea = async (req, res) => {
-  const { proyecto } = req.body;
-
-  const existeProyecto = await Proyecto.findById(proyecto);
-
-  if (!existeProyecto) {
-    const error = new Error("El Proyecto no existe");
-    return res.status(404).json({ msg: error.message });
+const validarCampoTarea = (valor, minLength, maxLength, mensaje) => {
+  if (valor.trim().length < minLength) {
+    throw new Error(`El ${mensaje} debe contener mínimo ${minLength} caracteres`);
+  } else if (valor.trim().length > maxLength) {
+    throw new Error(`El ${mensaje} debe contener máximo ${maxLength} caracteres`);
   }
-
-  if (existeProyecto.creador.toString() !== req.usuario._id.toString()) {
-    const error = new Error("No tienes los permisos para añadir tareas");
-    return res.status(403).json({ msg: error.message });
+};
+const validarFechaEntrega = (fechaEntrega) => {
+  const fechaEntregaDate = new Date(fechaEntrega);
+  const fechaActual = new Date();
+  if (fechaEntregaDate < fechaActual) {
+    throw new Error('La fecha de entrega no puede ser menor a la fecha actual');
   }
+};
+
+
+const validarTarea = async (req, res, next) => {
+  const { nombre, descripcion, fechaEntrega } = req.body;
 
   try {
-    const tareaAlmacenada = await Tarea.create(req.body);
-    // Almacenar el ID en el proyecto
-    existeProyecto.tareas.push(tareaAlmacenada._id);
-    await existeProyecto.save();
-    res.json(tareaAlmacenada);
+    validarCampoTarea(nombre, 3, 40, 'nombre de la tarea');
+    validarCampoTarea(descripcion, 1, 100, 'descripción');
+    validarFechaEntrega(fechaEntrega);
+    next();
+  } catch (error) {
+    return res.status(403).json({ msg: error.message });
+  }
+};
+
+
+
+
+const agregarTarea = async (req, res) => {
+
+  try {
+    validarTarea(req, res, async () => {
+      const { proyecto } = req.body;
+
+      const existeProyecto = await Proyecto.findById(proyecto);
+
+      if (!existeProyecto) {
+        const error = new Error("El Proyecto no existe");
+        return res.status(404).json({ msg: error.message });
+      }
+
+      if (existeProyecto.creador.toString() !== req.usuario._id.toString()) {
+        const error = new Error("No tienes los permisos para añadir tareas");
+        return res.status(403).json({ msg: error.message });
+      }
+
+
+      const tareaAlmacenada = await Tarea.create(req.body);
+      // Almacenar el ID en el proyecto
+      existeProyecto.tareas.push(tareaAlmacenada._id);
+      await existeProyecto.save();
+      res.json(tareaAlmacenada);
+
+    })
   } catch (error) {
     console.log(error);
   }
+
 };
 
 const obtenerTarea = async (req, res) => {
@@ -46,31 +84,35 @@ const obtenerTarea = async (req, res) => {
 };
 
 const actualizarTarea = async (req, res) => {
-  const { id } = req.params;
-
-  const tarea = await Tarea.findById(id).populate("proyecto");
-
-  if (!tarea) {
-    const error = new Error("Tarea no encontrada");
-    return res.status(404).json({ msg: error.message });
-  }
-
-  if (tarea.proyecto.creador.toString() !== req.usuario._id.toString()) {
-    const error = new Error("Acción no válida");
-    return res.status(403).json({ msg: error.message });
-  }
-
-  tarea.nombre = req.body.nombre || tarea.nombre;
-  tarea.descripcion = req.body.descripcion || tarea.descripcion;
-  tarea.prioridad = req.body.prioridad || tarea.prioridad;
-  tarea.fechaEntrega = req.body.fechaEntrega || tarea.fechaEntrega;
 
   try {
-    const tareaAlmacenada = await tarea.save();
-    res.json(tareaAlmacenada);
+    validarTarea(req, res, async () => {
+      const { id } = req.params;
+
+      const tarea = await Tarea.findById(id).populate("proyecto");
+
+      if (!tarea) {
+        const error = new Error("Tarea no encontrada");
+        return res.status(404).json({ msg: error.message });
+      }
+
+      if (tarea.proyecto.creador.toString() !== req.usuario._id.toString()) {
+        const error = new Error("Acción no válida");
+        return res.status(403).json({ msg: error.message });
+      }
+
+      tarea.nombre = req.body.nombre || tarea.nombre;
+      tarea.descripcion = req.body.descripcion || tarea.descripcion;
+      tarea.prioridad = req.body.prioridad || tarea.prioridad;
+      tarea.fechaEntrega = req.body.fechaEntrega || tarea.fechaEntrega;
+      const tareaAlmacenada = await tarea.save();
+      res.json(tareaAlmacenada);
+
+    })
   } catch (error) {
     console.log(error);
   }
+
 };
 
 const eliminarTarea = async (req, res) => {
--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -5,12 +5,56 @@ import { emailRegistro, emailOlvidePassword } from "../helpers/email.js";
 
 const registrar = async (req, res) => {
   // Evitar registros duplicados
-  const { email } = req.body;
+  const { nombre, email, password  } = req.body;
   const existeUsuario = await Usuario.findOne({ email });
+  
 
   if (existeUsuario) {
     const error = new Error("Usuario ya registrado");
     return res.status(400).json({ msg: error.message });
+  }
+
+  const MIN_NOMBRE_LENGHT = 1;
+  const MAX_NOMBRE_LENGHT = 50;
+  if (nombre.trim().length < MIN_NOMBRE_LENGHT) {
+    const error = new Error(
+      `El nombre debe contener minimo ${MIN_NOMBRE_LENGHT} caracteres`
+    );
+    return res.status(403).json({ msg: error.message });
+  } else if (nombre.trim().length > MAX_NOMBRE_LENGHT) {
+    const error = new Error(
+      `El nombre debe contener maximo ${MAX_NOMBRE_LENGHT} caracteres`
+    );
+    return res.status(403).json({ msg: error.message });
+  }
+
+
+  const MIN_EMAIL_LENGHT = 1;
+  const MAX_EMAIL_LENGHT = 50;
+  if (email.trim().length < MIN_EMAIL_LENGHT) {
+    const error = new Error(
+      `El email debe contener minimo ${MIN_EMAIL_LENGHT} caracteres`
+    );
+    return res.status(403).json({ msg: error.message });
+  } else if (email.trim().length > MAX_EMAIL_LENGHT) {
+    const error = new Error(
+      `El email debe contener maximo ${MAX_EMAIL_LENGHT} caracteres`
+    );
+    return res.status(403).json({ msg: error.message });
+  }
+
+  const MIN_PASSWORDL_LENGHT = 8;
+  const MAX_PASSWORDL_LENGHT = 60;
+  if (password.trim().length < MIN_PASSWORDL_LENGHT) {
+    const error = new Error(
+      `El password debe contener minimo ${MIN_PASSWORDL_LENGHT} caracteres`
+    );
+    return res.status(403).json({ msg: error.message });
+  } else if (password.trim().length > MAX_PASSWORDL_LENGHT) {
+    const error = new Error(
+      `El password debe contener maximo ${MAX_PASSWORDL_LENGHT} caracteres`
+    );
+    return res.status(403).json({ msg: error.message });
   }
 
   try {
@@ -18,7 +62,6 @@ const registrar = async (req, res) => {
     usuario.token = generarId();
     await usuario.save();
 
-    // Enviar el email de confirmacion
     emailRegistro({
       email: usuario.email,
       nombre: usuario.nombre,
@@ -42,6 +85,34 @@ const autenticar = async (req, res) => {
     const error = new Error("El Usuario no existe");
     return res.status(404).json({ msg: error.message });
   }
+  const MIN_EMAIL_LENGHT = 1;
+  const MAX_EMAIL_LENGHT = 50;
+  if (email.trim().length < MIN_EMAIL_LENGHT) {
+    const error = new Error(
+      `El email debe contener minimo ${MIN_EMAIL_LENGHT} caracteres`
+    );
+    return res.status(403).json({ msg: error.message });
+  } else if (email.trim().length > MAX_EMAIL_LENGHT) {
+    const error = new Error(
+      `El email debe contener maximo ${MAX_EMAIL_LENGHT} caracteres`
+    );
+    return res.status(403).json({ msg: error.message });
+  }
+
+  const MIN_PASSWORDL_LENGHT = 8;
+  const MAX_PASSWORDL_LENGHT = 60;
+  if (password.trim().length < MIN_PASSWORDL_LENGHT) {
+    const error = new Error(
+      `El password debe contener minimo ${MIN_PASSWORDL_LENGHT} caracteres`
+    );
+    return res.status(403).json({ msg: error.message });
+  } else if (password.trim().length > MAX_PASSWORDL_LENGHT) {
+    const error = new Error(
+      `El password debe contener maximo ${MAX_PASSWORDL_LENGHT} caracteres`
+    );
+    return res.status(403).json({ msg: error.message });
+  }
+  
 
   // Comprobar si el usuario esta confirmado
   if (!usuario.confirmado) {
@@ -83,12 +154,27 @@ const confirmar = async (req, res) => {
 
 const olvidePassword = async (req, res) => {
   const { email } = req.body;
+
+  const MIN_EMAIL_LENGHT = 1;
+  const MAX_EMAIL_LENGHT = 50;
+  if (email.trim().length < MIN_EMAIL_LENGHT) {
+    const error = new Error(
+      `El email debe contener minimo ${MIN_EMAIL_LENGHT} caracteres`
+    );
+    return res.status(403).json({ msg: error.message });
+  } else if (email.trim().length > MAX_EMAIL_LENGHT) {
+    const error = new Error(
+      `El email debe contener maximo ${MAX_EMAIL_LENGHT} caracteres`
+    );
+    return res.status(403).json({ msg: error.message });
+  }
+  
   const usuario = await Usuario.findOne({ email });
   if (!usuario) {
     const error = new Error("El Usuario no existe");
     return res.status(404).json({ msg: error.message });
   }
-
+  
   try {
     usuario.token = generarId();
     await usuario.save();
